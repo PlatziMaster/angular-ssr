e40f0ab27e4453847e3b2cd4a236022092052f7f
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-06 07:33:17 -0400

    :construction: Add support to ssr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ssr';
+
+  constructor(
+    private meta: Meta,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.meta.addTag({
+      name: 'description',
+      content: 'Take GitHub to the command line'
+    });
+    if (isPlatformBrowser(this.platformId)) {
+      navigator.geolocation.getCurrentPosition((res) => {
+        console.log(res);
+      })
+    }
+    
+  }
 }
